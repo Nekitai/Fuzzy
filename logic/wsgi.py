9c4762c4b6caeb94cf72f428ff5bfb993a0ba687
Nekitai <fajarap.0204@gmail.com>
2025-06-14 16:57:11 +0800

    deploy ulang

--- logic/wsgi.py
+++ logic/wsgi.py
@@ -1,12 +1,1 @@
-import sys
-import os
-
-# Tambahkan path logic ke PYTHONPATH
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, current_dir)
-
-# Sekarang aman untuk import
-from app import app
-
-# Untuk Vercel (eksport wsgi callable)
-application = app
+from app import app  # Karena app.py ada di folder yang sama
