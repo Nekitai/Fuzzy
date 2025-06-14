b51c03953a94c22d0ed25e251669907b840581dd
Nekitai <fajarap.0204@gmail.com>
2025-06-14 16:50:55 +0800

    deploy ulang

--- logic/wsgi.py
+++ logic/wsgi.py
@@ -1,1 +1,5 @@
-from app import app as application 
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # tambahkan direktori logic ke path
+from app import app  # Sekarang bisa ditemukan
