d86642399966ed70cdc024a726da560a25797058
Nekitai <fajarap.0204@gmail.com>
2025-06-14 17:43:03 +0800

    deploy ulang

--- fuzzy/src/pages/Input.tsx
+++ fuzzy/src/pages/Input.tsx
@@ -36,7 +36,7 @@ const App: React.FC = () => {
     };
 
     try {
-      const response = await fetch("http://fuzzy-six.vercel.app/fuzzy", {
+      const response = await fetch("/fuzzy", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
@@ -80,7 +80,7 @@ const App: React.FC = () => {
 
   const handleReset = async () => {
     try {
-      const response = await fetch("http://fuzzy-six.vercel.app/reset", {
+      const response = await fetch("/reset", {
         method: "POST",
       });
       if (response.ok) {
